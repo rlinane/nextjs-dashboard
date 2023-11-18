4f7c032083b5b920853577059e3dfa4261505e44
rlinane <rlinane@gmail.com>
2023-11-18 09:36:18 -0500

    added pagenation working

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -18,6 +18,7 @@ export default async function Page({
 
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
+  
   const totalPages = await fetchInvoicesPages(query);
 
   return (
